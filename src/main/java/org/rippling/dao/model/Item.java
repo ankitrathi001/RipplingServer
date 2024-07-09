eb5675239e051a77ae04b0fb46de4f319a1bb588
AnkitRathi <anrathi@AnkitRathis-MacBook-Pro.local>
2024-07-09 06:30:21 -0700

    Rippling Server

---
+++ src/main/java/org/rippling/dao/model/Item.java
@@ -0,0 +1,28 @@
+package org.rippling.dao.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank(message = "Item name must not be blank")
+    private String name;
+
+    private String phoneNumber;
+}
---
+++ src/main/java/org/rippling/web/errors/ErrorResponse.java
@@ -0,0 +1,19 @@
+package org.rippling.web.errors;
+
+//import jakarta.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+//@XmlRootElement(name = "error")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ErrorResponse {
+
+    private String message;
+    private List<String> details;
+}
